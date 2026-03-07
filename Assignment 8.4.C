a4224237ce54a3e5864466106640cd72312f7023
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-07 23:59:40 +0530

    Create Assignment 8.4.C

---
+++ Assignment 8.4.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{ 
+  int a=1,x;
+  printf("Enter a number");
+  scanf("%d",&x);
+  while(a<=x) 
+  {
+   printf("%d\n",a);
+   a=a+2;
+  }
+    return 0;
+}
+
+
+RUN____
+
+Enter a number20
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+
+[Process completed - press Enter]
