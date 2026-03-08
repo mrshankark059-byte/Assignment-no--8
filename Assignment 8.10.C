4b70822cadb1a64f31c1c878db828782cd899021
mrshankark059-byte <mrshankark059@gmail.com>
2026-03-08 10:03:45 +0530

    Update Assignment 8.10.C

--- Assignment 8.10.C
+++ Assignment 8.10.C
@@ -1,1 +1,35 @@
 Q. 10. Write a program to print a table of N.
+
+#include <stdio.h>
+int main()
+{ 
+  int a=1,x;
+  printf("Enter a number");
+  scanf("%d",&x);
+  while(a<=10) 
+  {
+   printf("%d\n",a*x);
+   a++;
+  }
+  
+    return 0;
+}
+
+
+
+RUN______
+
+
+Enter a number7
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+
+[Process completed - press Enter]
